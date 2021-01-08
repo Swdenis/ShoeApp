5d5158cd2aa5731121ab85204747ad43198ac2b4
Swdenis <swdenis@mail.ru>
2021-01-08 12:24:41 +0100

    * .gitignore: Initial commit
    * ShoeApp.sln:
    * Shoe.cs:
    * Style.cs:
    * Program.cs:
    * ShoeCloset.cs:
    * ShoeApp.csproj:

---
+++ ShoeApp/Shoe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ShoeApp
+{
+    class Shoe
+    {
+        public Style Style
+        {
+            get; private set;
+        }
+        public string Color
+        {
+            get; private set;
+        }
+        public Shoe(Style style, string color)
+        {
+            Style = style;
+            Color = color;
+        }
+        public string Description
+        {
+            get { return $"A {Color} {Style}"; }
+        }
+    }
+}
---
+++ ShoeApp/Style.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ShoeApp
+{
+    public enum Style
+    {
+        Sneaker,
+        Loafer,
+        Sandal,
+        Flipflop,
+        Wingtip,
+        Clog,
+    }
+}
